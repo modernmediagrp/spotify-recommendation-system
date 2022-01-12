095e9ce67254404d2aa324ad3a89da7d2a224092
Paul Lindquist <paullindquist@fastmail.com>
2022-01-12 09:35:26 -0600

    jan12 9:35am

---
+++ spotify_authorization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+"""
+Define an authorization function to run Spotify API.
+Utilizing the Tekore (https://github.com/felix-hilden/tekore)
+to navigate Spotify's API.
+"""
+
+import tekore as tk
+
+def authorize():
+    client_id = '4eb9684aba474efa92f55be38917497e'
+    client_secret = '80f447a4ac9a40ef99dac84fc345e3b9'
+    app_token = tk.request_client_token(client_id, client_secret)
+    return tk.Spotify(app_token)
+
